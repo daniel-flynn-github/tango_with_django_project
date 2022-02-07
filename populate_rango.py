225e303ddd27c7d9df2f6f3e3d8626f83d17a32f
Daniel Flynn <2469113f@student.gla.ac.uk>
2022-02-07 18:51:17 +0000

    Chapter 6 Complete

--- populate_rango.py
+++ populate_rango.py
@@ -5,6 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import django
 django.setup()
 
+
 from rango.models import Category, Page
 
 def populate():
@@ -15,25 +16,33 @@ def populate():
  # through each data structure, and add the data to our models.
 	python_pages = [
 		{'title': 'Official Python Tutorial',
-		  'url':'http://docs.python.org/3/tutorial/'},
+		  'url':'http://docs.python.org/3/tutorial/',
+		  'views': 1},
 		{'title':'How to Think like a Computer Scientist',
-		'url':'http://www.greenteapress.com/thinkpython/'},
+		'url':'http://www.greenteapress.com/thinkpython/',
+		'views': 2},
 		{'title':'Learn Python in 10 Minutes',
-		'url':'http://www.korokithakis.net/tutorials/python/'} ]
+		'url':'http://www.korokithakis.net/tutorials/python/',
+		'views': 3} ]
 
 	django_pages = [
 		{'title':'Official Django Tutorial',
-		'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+		'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+		'views': 4},
 		{'title':'Django Rocks',
-		 'url':'http://www.djangorocks.com/'},
+		 'url':'http://www.djangorocks.com/',
+		 'views': 5},
 		{'title':'How to Tango with Django',
-		'url':'http://www.tangowithdjango.com/'} ]
+		'url':'http://www.tangowithdjango.com/',
+		'views': 6} ]
 		
 	other_pages = [
 		{'title':'Bottle',
-		'url':'http://bottlepy.org/docs/dev/'},
+		'url':'http://bottlepy.org/docs/dev/',
+		'views': 7},
 		{'title':'Flask',
-		'url':'http://flask.pocoo.org'} ]
+		'url':'http://flask.pocoo.org',
+		'views': 8} ]
 	
 	cats = {'Python': {'pages': python_pages,'views':128,'likes':64},
 		'Django': {'pages': django_pages,'views':64,'likes':32},
@@ -48,7 +57,7 @@ def populate():
 		likes = cat_data['likes']
 		c = add_cat(cat,views,likes)
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 # Print out the categories we have added.
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
