2f924d99034170c2f4cb294306fd2e6e4f287206
Daniel Flynn <2469113f@student.gla.ac.uk>
2022-02-07 15:34:45 +0000

    Chapter 4 Complete

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	content = 'Rango says hey there partner!' + '<a href="/rango/about/">About</a>'
-	return HttpResponse(content)
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	content =  'Rango says here is the about page.' + '<a href="/rango/">Index</a>'
-	return HttpResponse(content)
+	context_dict = {'boldmessage': 'Daniel Flynn.'}
+	return render(request, 'rango/about.html', context=context_dict)
